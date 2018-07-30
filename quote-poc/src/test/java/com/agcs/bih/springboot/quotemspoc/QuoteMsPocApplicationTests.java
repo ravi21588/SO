7aba040852f709cdf2374bb13254d9fc8ce33008
ravinarayanan arunachalam <extern.arunachalam_ravinarayanan@allianz.de>
2018-07-30 12:16:24 +0200

    Initial commit
    Initial commit

---
+++ quote-poc/src/test/java/com/agcs/bih/springboot/quotemspoc/QuoteMsPocApplicationTests.java
@@ -0,0 +1,19 @@
+package com.agcs.bih.springboot.quotemspoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Empty test to test that Spring Boot can startup
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuoteMsPocApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
