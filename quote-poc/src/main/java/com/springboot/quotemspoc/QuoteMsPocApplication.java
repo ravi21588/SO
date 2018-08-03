949ca3aef9f900d13117843b912efe386d7c0c64
ravinarayanan arunachalam <extern.arunachalam_ravinarayanan@allianz.de>
2018-08-03 14:59:59 +0200

    Updated camel route
    Updated camel route

---
+++ quote-poc/src/main/java/com/springboot/quotemspoc/QuoteMsPocApplication.java
@@ -0,0 +1,17 @@
+package com.springboot.quotemspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Basic Spring Boot application that loads the Camel XML file using @ImportResource
+ */
+@SpringBootApplication
+@ImportResource("classpath:camel-context.xml")
+public class QuoteMsPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteMsPocApplication.class, args);
+	}
+}
--- quote-poc/src/test/java/com/springboot/quotemspoc/QuoteMsPocApplicationTests.java
+++ quote-poc/src/test/java/com/springboot/quotemspoc/QuoteMsPocApplicationTests.java
@@ -1,4 +1,4 @@
-package com.agcs.bih.springboot.quotemspoc;
+package com.springboot.quotemspoc;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
